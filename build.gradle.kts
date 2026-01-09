e5461c9ca99e54cf3e891982e29819c0483f872e
Maria Tigina <tiginamaria@gmail.com>
2026-01-09 20:01:55 +0100

    Add koog dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    alias(libs.plugins.kotlin.serialization)
 }
 
-group = "ai.koog"
+group = "aai.agent.coding"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://packages.jetbrains.team/maven/p/grazi/grazie-platform-public")
 }
 
 dependencies {
+    implementation(libs.koog.agents)
+    implementation(libs.logback.classic)
     testImplementation(kotlin("test"))
 }
 
